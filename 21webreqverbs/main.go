33bf30293e7cce058c504afe3f817bfcf112ddea
GitUtk <utkarshyadavun@gmail.com>
2026-08-14 01:27:46 +0530

    POST Requests and Form Requests

--- 21webreqverbs/main.go
+++ 21webreqverbs/main.go
@@ -4,12 +4,54 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strings"
 )
 
 func main() {
 	fmt.Println("Welcome to web verb video - LCO")
-	defer PerformGetRequest()
+	// defer PerformGetRequest()
+	// defer PerformPostJsonRequest()
+	defer PerformPostFormRequest()
+}
+
+func PerformPostJsonRequest() {
+	const myurl = "http://localhost:3000/post"
+
+	// fake json payload
+	requestBody := strings.NewReader(`
+		{
+			"course":"Let's go with golang",
+			"price":0,
+			"platform":"github.com"
+		}
+	`)
+	res, err := http.Post(myurl, "application/json", requestBody)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	content, err := ioutil.ReadAll(res.Body)
+	fmt.Println(string(content))
+}
+
+func PerformPostFormRequest() {
+	const myurl = "http://localhost:3000/postform"
+
+	//formdata
+	data := url.Values{}
+	data.Add("Firstname", "Utkarsh")
+	data.Add("LastName", "Yadav")
+	data.Add("email", "utkarsh@go.dev")
+
+	res, err := http.PostForm(myurl, data)
+	if err != nil {
+		panic(err)
+	}
+	content, err := ioutil.ReadAll(res.Body)
+	fmt.Println(string(content))
+
+	defer res.Body.Close()
 }
 
 func PerformGetRequest() {
--- server.js
+++ server.js
@@ -1,13 +1,60 @@
 const http = require("http");
+const querystring = require("querystring");
 
 const server = http.createServer((req, res) => {
   if (req.method === "GET" && req.url === "/get") {
     res.end("GET endpoint");
+    return;
   }
 
   if (req.method === "POST" && req.url === "/post") {
-    res.end("POST endpoint");
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      console.log("Received data:");
+      console.log(body);
+
+      res.end("Received Data : " + body);
+    });
+
+    return;
   }
+
+  // Handle Go http.PostForm request
+  if (req.method === "POST" && req.url === "/postform") {
+    let body = "";
+
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    req.on("end", () => {
+      const formData = querystring.parse(body);
+
+      console.log("Received form data:");
+      console.log(formData);
+
+      res.writeHead(200, {
+        "Content-Type": "application/json",
+      });
+
+      res.end(
+        JSON.stringify({
+          message: "Form data received successfully",
+          data: formData,
+        })
+      );
+    });
+
+    return;
+  }
+
+  res.statusCode = 404;
+  res.end("Not Found");
 });
 
 server.listen(3000, () => {
